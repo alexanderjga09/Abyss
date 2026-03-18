024d86b51a1fbcb495d3e46638c20762961accf0
alexanderjga09 <alexanderjga09@gmail.com>
2026-03-17 23:48:06 -0400

    Waa

--- fish.py
+++ fish.py
@@ -18,6 +18,7 @@ class Fish:
             prices = json.load(f)
 
         self.rarity = prices["fishes"][self.name]["rarity"]
+        self.thumbnail = prices["fishes"][self.name]["thumbnail"]
         self.mutation = mutation.capitalize()
         self.dead = dead
 
@@ -45,6 +46,9 @@ class Fish:
         with open("data/prices.json") as f:
             fishes = json.load(f)
 
+        with open("data/mutations.json") as f:
+            mutations = json.load(f)
+
         if "meat" not in self.name and "head" not in self.name:
             price = self.price() * (0.01 if self.mutation != "" else 0.02)
             pr_hour = (
@@ -65,6 +69,49 @@ class Fish:
                 )
             ) * 60
 
+            from quickchart import QuickChart
+
+            qc = QuickChart()
+            qc.width = 500
+            qc.height = 300
+            qc.version = "3"
+
+            # Config can be set as a string or as a nested dict
+            qc.config = {
+                "type": "radar",
+                "data": {
+                    "labels": [
+                        "$/hr",
+                        "PriceRoe(pts)",
+                        "Kg/hr(pts)",
+                        "Mutation(pts)",
+                    ],
+                    "datasets": [
+                        {
+                            "backgroundColor": "rgba(54, 162, 235, 0.5)",
+                            "borderColor": "rgb(54, 162, 235)",
+                            "data": [
+                                pr_hour,
+                                price * 50,
+                                we_hour * 50,
+                                (mutations[self.mutation] * 250)
+                                if self.mutation != ""
+                                else 50,
+                            ],
+                            "label": "Stats",
+                        },
+                    ],
+                },
+                "options": {
+                    "spanGaps": False,
+                    "elements": {
+                        "line": {
+                            "tension": 0.000001,
+                        },
+                    },
+                },
+            }
+
             return {
                 "price_roe": round(price, 0),
                 "roe_per_hour": round(pr_hour, 0),
@@ -72,4 +119,5 @@ class Fish:
                 "weight_roe": round(weight, 2),
                 "weight_per_hour": round(we_hour, 2),
                 "weight_per_day": round(we_hour * 24, 2),
+                "chart": qc.get_url(),
             }
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 import discord
+import roman
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -22,14 +23,19 @@ def main():
     @client.slash_command(name="info_fish", description="placeholder")
     async def info_fish(
         interaction: discord.Interaction,
-        name: str,
-        weight: float,
-        stars: int,
-        mutation: str = "",
+        name=discord.Option(str, "The name of the fish"),
+        weight=discord.Option(float, "The weight of the fish"),
+        stars=discord.Option(int, "The number of stars the fish has"),
+        mutation=discord.Option(str, "The mutation of the fish", default=""),
         dead: bool = False,
-        roe_speed_level: int = 0,
     ):
-        fish = fs.Fish(name, weight, stars, mutation, dead)
+        fish = fs.Fish(
+            name,
+            round(weight if "," in str(weight) else (weight * 0.1), 1),
+            stars,
+            mutation,
+            dead,
+        )
 
         embed = discord.Embed(
             title=fish.name,
@@ -38,14 +44,44 @@ def main():
         )
         embed.add_field(
             name="Details:",
-            value=f"**Stars:** {':star: ' * fish.stars}\n**Rarity:** {fish.rarity}\n**Mutation:** {fish.mutation}\n**Dead:** {fish.dead}",
+            value=f"**Stars:** {((':star: ' * fish.stars) + f'`x{0.25 + (0.25 * fish.stars)}`') if not fish.dead else ':skull: `x0.2`'} | **Rarity:** {fish.rarity}\n**Mutation:** {fish.mutation}",
+        )
+        embed.set_thumbnail(url=fish.thumbnail)
+
+        await interaction.response.send_message(embed=embed)
+
+    @client.slash_command(name="prod_fish", description="placeholder")
+    async def prod_fish(
+        interaction: discord.Interaction,
+        name=discord.Option(str, "The name of the fish"),
+        weight=discord.Option(float, "The weight of the fish"),
+        stars=discord.Option(int, "The number of stars the fish has"),
+        mutation=discord.Option(str, "The mutation of the fish", default=""),
+        dead: bool = False,
+        rsl: int = 0,
+    ):
+        fish = fs.Fish(
+            name,
+            round(weight if "," in str(weight) else (weight * 0.1), 1),
+            stars,
+            mutation,
+            dead,
+        )
+        embed = discord.Embed(
+            title=f"{fish.name} (${fish.price()})",
+            description=f"**$/hr:** {fish.production(rsl=rsl)['roe_per_hour']}\n**Kg/hr:** {fish.production(rsl=rsl)['weight_per_hour']}\n"
+            if fish.production(rsl=rsl) is not None
+            else "-# Can't do that",
+            color=discord.Color.yellow(),
         )
         embed.add_field(
-            name="Production:",
-            value=f"**$/hr:** {fish.production(rsl=roe_speed_level)['roe_per_hour']}\n**Kg/hr:** {fish.production(rsl=roe_speed_level)['weight_per_hour']}\n"
-            if fish.production(rsl=roe_speed_level) is not None
-            else "-# Can't do that",
+            name="Details:",
+            value=f"**Stars:** {((':star: ' * fish.stars) + f'`x{0.25 + (0.25 * fish.stars)}`') if not fish.dead else ':skull: `x0.2`'} | **Rarity:** {fish.rarity}\n**Mutation:** {fish.mutation}",
         )
+
+        embed.set_thumbnail(url=fish.thumbnail)
+        embed.set_image(url=fish.production(rsl=rsl)["chart"])
+        embed.set_footer(text=f"Roe Speed Level: {roman.toRoman(rsl)}")
 
         await interaction.response.send_message(embed=embed)
 
